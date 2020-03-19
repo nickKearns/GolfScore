4d67044690a4febda1bd307696eb1abe38bd15fc
nickKearns <nickkearns133@gmail.com>
2020-03-19 14:33:42 -0400

    added new home screen

--- controllers/HomeVC.swift
+++ controllers/HomeVC.swift
@@ -1,69 +1,84 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  GolfScore
 //
-//  Created by Nicholas Kearns on 3/17/20.
+//  Created by Nicholas Kearns on 3/19/20.
 //  Copyright © 2020 Nicholas Kearns. All rights reserved.
 //
 
 import UIKit
 
-class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
 
-    let scoresTable: UITableView = {
-       let scoresTable = UITableView()
-       scoresTable.translatesAutoresizingMaskIntoConstraints = false
-       scoresTable.rowHeight = 75
-       return scoresTable
+class HomeVC: UIViewController {
+    
+    
+    let logoImage: UIImageView = {
+        let logoImage = UIImageView()
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        logoImage.image = UIImage(named: "logo")
+        return logoImage
     }()
     
+    let newRoundButton: UIButton = {
+        let newRoundButton = UIButton()
+        newRoundButton.translatesAutoresizingMaskIntoConstraints = false
+        newRoundButton.setTitle("New Round", for: .normal)
+        newRoundButton.layer.cornerRadius = 10
+        newRoundButton.layer.borderWidth = 2
+        newRoundButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        newRoundButton.setTitleColor(.black, for: .normal)
+        newRoundButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        newRoundButton.titleEdgeInsets = UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+       return newRoundButton
+    }()
     
+    let pastRoundsButton: UIButton = {
+        let pastRoundsButton = UIButton()
+        pastRoundsButton.translatesAutoresizingMaskIntoConstraints = false
+        pastRoundsButton.setTitle("Past Rounds", for: .normal)
+        pastRoundsButton.layer.cornerRadius = 10
+        pastRoundsButton.layer.borderWidth = 2
+        pastRoundsButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        pastRoundsButton.setTitleColor(.black, for: .normal)
+        pastRoundsButton.titleEdgeInsets = UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+        return pastRoundsButton
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print("view loaded")
-        setupTable()
-        // Do any additional setup after loading the view.
+        setupView()
     }
     
-    func setupTable() {
-        self.view.addSubview(scoresTable)
+    
+    
+    func setupView() {
+        self.view.backgroundColor = #colorLiteral(red: 0, green: 0.6096050739, blue: 0.3671522737, alpha: 1)
+        
+        self.view.addSubview(logoImage)
         NSLayoutConstraint.activate([
-            scoresTable.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            scoresTable.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            scoresTable.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-            scoresTable.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
+            logoImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            logoImage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -150),
+            logoImage.widthAnchor.constraint(equalToConstant: 200),
+            logoImage.heightAnchor.constraint(equalToConstant: 200)
         ])
-        scoresTable.delegate = self
-        scoresTable.dataSource = self
-        scoresTable.keyboardDismissMode = .onDrag
         
-        scoresTable.register(UINib(nibName: "HoleTableCell", bundle: nil), forCellReuseIdentifier: "HoleTableCell")
+        self.view.addSubview(newRoundButton)
+        newRoundButton.backgroundColor = #colorLiteral(red: 0.2506845593, green: 0.7617723346, blue: 0.9665134549, alpha: 1)
+        NSLayoutConstraint.activate([
+            newRoundButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -100),
+            newRoundButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -200),
+            newRoundButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.40)
+        ])
         
-    }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Current Round"
-    }
-    
-    
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        9
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell( withIdentifier: HoleTableCell.identifier, for: indexPath) as! HoleTableCell
-        cell.configure(numberHole: indexPath.row + 1)
-        return cell
+        self.view.addSubview(pastRoundsButton)
+        pastRoundsButton.backgroundColor = #colorLiteral(red: 0.2642833591, green: 0.7614423037, blue: 0.9702764153, alpha: 1)
+        NSLayoutConstraint.activate([
+            pastRoundsButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 100),
+            pastRoundsButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -200),
+            pastRoundsButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.40)
+        
+        ])
     }
     
 }
-
---
+++ controllers/PastVC.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  GolfScore
+//
+//  Created by Nicholas Kearns on 3/17/20.
+//  Copyright © 2020 Nicholas Kearns. All rights reserved.
+//
+
+import UIKit
+
+class PastVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+    let scoresTable: UITableView = {
+       let scoresTable = UITableView()
+       scoresTable.translatesAutoresizingMaskIntoConstraints = false
+       scoresTable.rowHeight = 75
+       return scoresTable
+    }()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTable()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupTable() {
+        self.view.addSubview(scoresTable)
+        NSLayoutConstraint.activate([
+            scoresTable.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scoresTable.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scoresTable.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            scoresTable.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
+        ])
+        scoresTable.delegate = self
+        scoresTable.dataSource = self
+        scoresTable.keyboardDismissMode = .onDrag
+        
+        scoresTable.register(UINib(nibName: "HoleTableCell", bundle: nil), forCellReuseIdentifier: "HoleTableCell")
+        
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Current Round"
+    }
+    
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        9
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell( withIdentifier: HoleTableCell.identifier, for: indexPath) as! HoleTableCell
+        cell.configure(numberHole: indexPath.row + 1)
+        return cell
+    }
+    
+    
+    
+}
+
